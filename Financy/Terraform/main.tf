719127fd9bd062ba5de076aaae7ebc2ab3aceb12
Artur Babii <47463203+enumerable-entity@users.noreply.github.com>
2022-04-15 22:10:49 +0200

    Update main.tf

--- Financy/Terraform/main.tf
+++ Financy/Terraform/main.tf
@@ -10,3 +10,15 @@ terraform {
 provider "aws" {
 region = "eu-central-1"
 }
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-enumerable-entity"
+    key    = "dev/network/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+output "newwork-state" {
+  value = data.terraform_remote_state.network.outputs.vpc_id  # value interchange through outputs in tfstate file. One person make outputs, another read outputs from tfstate file in s3
+}
