6c15eb2799f5722b4661637c4a643be6d0823506
Enumerable-entity <alfred.dc.alpha@gmail.com>
2022-04-17 21:17:51 +0200

    [WIP]

---
+++ Financy/Terraform/dev/config.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.1.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  cloud {
+    organization = "enumerable-entity"
+    workspaces {
+      name = "Terraform-IaC-scripts"
+    }
+  }
+#   backend "s3" {
+#     bucket = "terraform-state-enumerable-entity"
+#     key    = "financy/dev"
+#     region = var.default_region
+#   }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
---
+++ Financy/Terraform/dev/variables.tf
@@ -0,0 +1,7 @@
+variable "default_region" {
+
+  type        = string
+  default     = "eu-central-1"
+  description = "This is the default AWS region for whole project"
+
+}
